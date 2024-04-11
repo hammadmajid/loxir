de5afe1ebed1a21d14091a5e6cacb01c1bb3a92d
Hammad Majid <hammadmajid@proton.me>
2024-04-11 19:13:17 +0500

    create Token struct to hold a token and TokenKind enum for holding all the types of tokens accepted

---
+++ src/lexer.rs
@@ -0,0 +1,57 @@
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub struct Token {
+    lexeme: String,
+    kind: TokenKind,
+}
+
+#[derive(Debug, PartialEq)]
+enum TokenKind {
+    // Single-character tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals.
+    Identifier,
+    String,
+    Number,
+
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    Eof,
+}
